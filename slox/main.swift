3243d936f7eb802da1275b6a7c02216e774c466d
Peter Olsen <pto@me.com>
2019-12-31 21:06:12 -0500

    Initial Commit

---
+++ slox/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  slox
+//
+//  Created by Peter Olsen on 12/31/19.
+//  Copyright © 2019 Peter Olsen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
